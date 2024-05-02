4153ec589c4138e25af8b3e5ac2dd8b7cda9e588
Jesus Raad <jesus.raad@students.gammatech.school>
2024-05-02 19:09:29 +0200

    save

--- funciones.js
+++ funciones.js
@@ -20,7 +20,7 @@ function Fahrenheit(c) {
 //   3.- Escribe una función que acepte dos números, un mínimo y un máximo, y devuelva un número aleatorio entre dichos números.
 //    Para ello, tendrás que descubrir cómo funciona el método Math.random()
 
-function getRandomArbitrary(min, max) {
+function between(min, max) {
     return Math.random() * (max - min) + min;
   }
 
@@ -38,6 +38,23 @@ function areaCircle(r) {
 // -Nombre de una ciudad.
 // -Un trabajo. La función devolverá "Trabajarás en <<ciudad>>, trabajando como <<trabajo>>, y tendrás <<número de hijos>> hijos con <<pareja>>.".
 // Deberás usar template literals para ello
+let sons=[1,2,3,4,5]
+let culish=["Daniela", "Diana", "Mariana", "Johana", "Valeria"]
+let city=["Caracas", "Cartagena", "Margarita", "Medellin", "Venise"]
+let job=["Desarrollador", "Astronauta", "Presidente", "Youtuber", "Influencer"]
+
+
+
 function future(sons,culish,city,job) {
-   return `Trabajarás en ${city}, trabajando como ${job}, y tendrás ${sons} hijos con ${culish}.`
+
+  const randomSonIndex = Math.floor(Math.random() * sons.length);
+  const randomCulishIndex = Math.floor(Math.random() * culish.length);
+  const randomCityIndex = Math.floor(Math.random() * city.length);
+  const randomJobIndex = Math.floor(Math.random() * job.length);
+//lo de arriba es para sacar el numero aleatorio de la posicion en el array
+  
+
+// establece el numero dentro del array para ubicarlo dentro en la posicion correspondiente
+   return `Trabajarás en ${city[randomCityIndex]} ,trabajando como ${job[randomJobIndex]} , y tendrás ${sons[randomSonIndex]} hijos con ${culish[randomCulishIndex]} .`; 
 }
+
--- loops.js
+++ loops.js
@@ -1,10 +1,25 @@
-// 1.- 
+// 1.-
 //         1.1.- Itera del 0 al 10 usando un bucle "for" e imprime cada número por consola.
 //         1.2.-  Haz lo mismo con un bucle "while".
 //         1.3.- Haz lo mismo del 83 al 24 (bucle "for" y bocle "while").
+console.log(`EJERCICIO 1`);
 
-
-
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+for (let i = 83; i >= 24; i--) {
+  console.log(i);
+}
+i = 83;
+while (i >= 24) {
+  console.log(i);
+  i--;
+}
 
 // 2.- Crea la variable "num" y haz que un bucle "for" imprima la tabla de multiplicar de ese número (del 0 al 10) en la consola, siguiendo el siguiente formato:
 
@@ -12,8 +27,11 @@
 // num x 1 = num * 1
 // num x 2 = num * 2
 // etc..
-
-
+console.log(`EJERCICIO 2`);
+let num = 2;
+for (let i = 0; i <= 10; i++) {
+  console.log(num + `x${i}=` + i * num);
+}
 
 // 3.- Imprime por consola el siguiente patrón:
 //  i    i**2   i**3
@@ -29,24 +47,68 @@
 //  8    64    512
 //  9    81    729
 //  10   100   1000
+console.log(`EJERCICIO 3`);
 
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i} ${i ** 2} ${i ** 3}`);
+}
 
 // 4.- Imprime por consola, , la suma de todos los números del 0 al 10.
 
-
-
-
+console.log(`EJERCICIO 4`);
+let sum = 0;
+for (let i = 0; i <= 10; i++) {
+  sum = sum + i;
+}
+console.log(sum);
 // 5.-
 //          5.1.- Imprime por consola los números del 0 hasta el 100, pero sólo los que estén comprendidos entre el 10 y 23, entre el 56 y 62.
 //          5.2.- Luego haz otro log que muestre solo los números divisibles entre 5 o 7
+console.log(`EJERCICIO 5`);
 
+for (let i = 0; i <= 100; i++) {
+  if (i >= 0 && i < 10) {
+    continue;
+  }
+  if (i > 23 && i < 56) {
+    continue;
+  }
+  if (i > 62 && i <= 100) {
+    continue;
+  } else {
+    console.log(i);
+  }
+}
 
-
-
-// 6.- Pídele al usuario que introduzca un número e imprime por consola: "La suma de todos los números pares del 0 al <<num>> es <<result>>, 
+// 6.- Pídele al usuario que introduzca un número e imprime por consola: "La suma de todos los números pares del 0 al <<num>> es <<result>>,
 // y la suma de todos los números impares del 0 al <<num>> es <<result>>."
 
+console.log(`EJERCICIO 6`);
 
+let num1 = prompt("escriba un numero");
+
+let acomuladorPar = 0;
+let acomuladorImpar = 0;
+
+for (let i = 0; i <= parseInt(num1); i++) {
+  if (i % 2 === 0) {
+    acomuladorImpar = acomuladorImpar + i;
+
+    continue;
+  }
+  if (i % 2 !== 0) {
+    acomuladorPar = acomuladorPar + i;
+
+    continue;
+  }
+}
+console.log(
+  `La suma de todos los números impares del 0 al ${num1} es ${acomuladorPar}`
+);
+console.log(
+  `La suma de todos los números impares del 0 al ${num1} es ${acomuladorImpar}`
+);
+// console.log(`La suma de todos los números pares del 0 al ${num1} es ${acomulador}`);
 
 // 7.- Imprime el siguiente patrón por consola:
 
@@ -57,8 +119,10 @@
 // #####
 // ######
 // #######
-
-
+console.log(`EJERCICIO 7`);
+for (let i = ""; i.length <= 10; i += "#") {
+  console.log(i);
+}
 
 // 8.- Imprime por consola el siguiente patrón:
 //                ##
@@ -74,3 +138,14 @@
 //              ######
 //               ####
 //                ##
+console.log(`EJERCICIO 8`);
+
+
+for (let i = ""; i.length <= 19; i += "##") {
+  console.log(i);
+}
+
+for (let i = "####################"; i.length > 0; i =i.slice(2)) {
+  console.log(i);
+}
+
